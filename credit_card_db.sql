cca4e7ee2359cf9900a99b8a3ed3bd6ca9a7d6a7
DexterCosmos <dexter.cosmos@outlook.com>
2025-03-16 16:13:24 +0530

    Initial Commit

--- credit_card_db.sql
+++ credit_card_db.sql
@@ -1,1 +1,57 @@
 SELECT * FROM credit_card;
+
+
+1. -- Total Expenditure per City
+
+SELECT City, SUM(Amount) AS Total_Expenditure
+FROM credit_card
+GROUP BY City
+ORDER BY Total_Expenditure DESC;
+
+
+2. -- Gender-wise Expenditure
+
+SELECT Gender, SUM(Amount) AS Total_Expenditure
+FROM credit_card
+GROUP BY Gender
+ORDER BY Total_Expenditure DESC;
+
+
+3. -- Card Type Usage
+
+SELECT 
+    `Card Type`,
+    COUNT(*) AS UsageCount, 
+    SUM(`Amount`) AS TotalSpend
+FROM 
+    credit_card
+GROUP BY 
+    `Card Type`
+ORDER BY 
+    UsageCount DESC;
+
+4. -- Expenditure by Expense Type
+
+SELECT 
+    `Exp Type`,
+    COUNT(*) AS UsageCount, 
+    SUM(`Amount`) AS TotalSpend
+FROM 
+    credit_card
+GROUP BY 
+    `Exp Type`
+ORDER BY 
+    UsageCount DESC;
+
+5. -- Monthly Expenditure
+
+SELECT 
+    MONTH(`Month`) AS Month,
+    SUM(`Amount`) AS TotalSpend
+FROM 
+    credit_card
+GROUP BY 
+    Month
+ORDER BY 
+    Month;
+
